2fc992b0602283bf10e0d1c726f69731be86a728
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-16 19:56:58 +0600

    made requiered changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,24 @@
-const path    = require("path")
-const webpack = require("webpack")
+// webpack.config.js
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'development', // or 'production'
+  devtool: 'source-map',
   entry: {
-    application: "./app/javascript/application.js"
-  },
-  output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    application: './app/javascript/application.js',
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: ['babel-loader'],
       },
     ],
   },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+  output: {
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map',
+    path: path.resolve(__dirname, 'app/assets/builds'),
+  },
+};
